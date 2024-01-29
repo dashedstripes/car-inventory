891970edae27e354dc7b682b8f7f30dbebf6f71f
Adam Gray <adam.gray@netlify.com>
2024-01-29 11:09:11 -0600

    add image for car page

--- app/cars/[slug]/page.tsx
+++ app/cars/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function Car({ params }: { params: { slug: string }}) {
 
   return (
     <div className="container mx-auto">
-      <img src={data.imgSrc} alt={data.make} />
+      <img className="my-4" src={`/.netlify/images?url=/images/${data.imgSrc}&q=80&w=1000&h=600&fit=cover`} alt={data.make} />
       <h1 className="font-bold text-2xl py-8">{data.make} {data.model}</h1>
       <p>{data.description}</p>
     </div>
